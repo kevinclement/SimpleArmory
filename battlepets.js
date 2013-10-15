52adb4e4a35f198e0f6edd30801d63a74871161e
Kevin Clement <kevin.clement@gmail.com>
2013-10-15 16:44:35 -0700

    Started adding battle pets

---
+++ battlepets.js
@@ -0,0 +1,122 @@
+var BattlePets = 
+[
+{
+  "name": "Pets",
+  "subcats": [
+    {
+      "name": "Outland",
+      "items": [
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62620",
+          "icon": "inv_misc_foot_centaur",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "68841",
+          "icon": "inv_pet_mechanicalraptorpet",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62621",
+          "icon": "spell_fire_felfire",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62555",
+          "icon": "ability_racial_flayer",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62627",
+          "icon": "ability_hunter_pet_netherray",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62625",
+          "icon": "inv_pet_cockroach",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62628",
+          "icon": "trade_archaeology_trolllizardfootcharm",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62638",
+          "icon": "ability_hunter_pet_spider",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62564",
+          "icon": "inv_mushroom_11",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        },
+        {
+          "spellid": null,
+          "allianceId": null,
+          "hordeId": null,
+          "itemId": null,
+          "creatureId": "62583",
+          "icon": "ability_hunter_pet_warpstalker",
+          "obtainable": true,
+          "allowableRaces": [],
+          "allowableClasses": null
+        }
+      ]
+    }
+   ]
+}]
