baf17e4a7a046a2737f27f1d16b200e6e3f3a617
Kevin Clement <kevin.clement@gmail.com>
2014-10-14 01:47:15 -0700

    Buttons for pet levels

--- v2/js/controllers.js
+++ v2/js/controllers.js
@@ -271,6 +271,28 @@ simpleArmoryControllers.controller('BattlePetsCtrl', ['$scope', 'MountsAndPetsSe
   battlePetsService.getItems("battlepets", "pets", "creatureId").then(function(items){
       $scope.items = items;
   });
+
+  $scope.qualityToBackground = function(item) {
+    var bgColor = "#fff";
+
+    switch(item.quality) {
+        case "poor":
+            bgColor = "#7F7F7F";
+            break;
+        case "common":
+            bgColor = "#F0F0F0";
+            break;
+        case "uncommon":
+            bgColor = "#22B14C";
+            break;
+        case "rare":
+            bgColor = "#3F48CC";
+            break;
+    }
+
+    return "background:" + bgColor;
+  }
+
 }]);
 
 simpleArmoryControllers.controller('CalendarCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
