7065fb4854f8dedaa067c1309797a2aad091c4ae
Kevin Clement <kevin.clement@gmail.com>
2014-10-15 22:43:27 -0700

    v2 converted to grunt

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,21 +8,54 @@
  *
  * Main module of the application.
  */
-angular
-  .module('simpleArmoryApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
+var simpleArmoryApp = angular.module('simpleArmoryApp', [
+  'ngRoute',
+  'simpleArmoryControllers',
+  'simpleArmoryFilters',
+  'simpleArmoryServices',
+  'ui.bootstrap'
+]);
+
+simpleArmoryApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/:region/:realm/:character/achievements/:category', {
+        templateUrl: 'views/achievements.html',
+        controller: 'AchievementsCtrl'
+      }).  
+      when('/:region/:realm/:character/collectable/battlepets', {
+        templateUrl: 'views/battlePets.html',
+        controller: 'BattlePetsCtrl'
+      }).  
+      when('/:region/:realm/:character/calendar', {
+        templateUrl: 'views/calendar.html',
+        controller: 'CalendarCtrl'
+      }).  
+      when('/:region/:realm/:character/collectable/companions', {
+        templateUrl: 'views/companions.html',
+        controller: 'CompanionsCtrl'
+      }). 
+      when('/:region/:realm/:character/collectable/mounts', {
+        templateUrl: 'views/mounts.html',
+        controller: 'MountsCtrl'
+      }).        
+      when('/:region/:realm/:character', {
+        templateUrl: 'views/overview.html',
+        controller: 'OverviewCtrl'
+      }).      
+      when('/:region/:realm/:character/reputation', {
+        templateUrl: 'views/reputation.html',
+        controller: 'ReputationCtrl'
+      }).          
+      when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      }).
+      when('/error', {
+        templateUrl: 'views/error.html',
+        controller: 'ErrorCtrl'
+      }).      
+      otherwise({
         redirectTo: '/'
       });
-  });
+  }]);
---
+++ app/scripts/filters.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Filters */
+angular.module('simpleArmoryFilters', []);
+
+/*
+simpleArmoryFilters.filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});
+*/
