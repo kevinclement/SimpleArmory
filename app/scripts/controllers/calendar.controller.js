7470e63e116cb481834bfe66f26410e0d8b24164
Kevin Clement <kevin.clement@gmail.com>
2014-10-18 22:15:18 -0700

    Build of divs for achievements

--- app/scripts/controllers/calendar.controller.js
+++ app/scripts/controllers/calendar.controller.js
@@ -6,14 +6,18 @@
         .module('simpleArmoryApp')
         .controller('CalendarCtrl' , CalendarCtrl);
 
-    function CalendarCtrl($scope, $sce) { 
-	   	$scope.totalForMonth = 23;
-    	$scope.totalPoints = '(15 points)';
-	
-		buildMonths(); 
+    function CalendarCtrl($scope, AchievementsService, $sce) { 
 
-		// Select the last month
-		$scope.selectedMonth = $scope.months[$scope.months.length - 1];
+    	 AchievementsService.getAchievements().then(function(achievements){
+	
+		   	$scope.totalForMonth = 23;
+    		$scope.totalPoints = '(15 points)';
+	
+			buildMonths(achievements); 
+
+			// Select the last month
+			$scope.selectedMonth = $scope.months[$scope.months.length - 1];
+        });
 
     	$scope.leftOneMonth = function() {
 
@@ -32,39 +36,62 @@
     	};
 
     	$scope.selectionChanged = function() {
-    		var cc = $(".curCalendar");
-    		cc.removeClass("curCalendar");
+    		var cc = $('.curCalendar');
+    		cc.removeClass('curCalendar');
     		cc.hide();
 
 			var newCC = $('#calendar' + $scope.selectedMonth.value);
-			newCC.addClass("curCalendar");
+			newCC.addClass('curCalendar');
 			newCC.show();
-    	}
+    	};
 
-    	function buildMonths() {
-    		var monthnames = [,'January','February','March','April','May','June','July','August','September','October','November','December'];
+    	function buildMonths(achievements) {
+    		var monthnames = [,'January','February','March','April','May','June',
+    						   'July','August','September','October','November','December'];
 			var months = [];
 			var today = new Date();
+			var achByMonths = {};
 
 			var calendarHtml = '';
 
+			// build a lookup of month to achievement list
+			angular.forEach(achievements, function(supercat) {
+				angular.forEach(supercat.categories, function(cat) {
+					angular.forEach(cat.zones, function(zone) {
+						angular.forEach(zone.achievements, function(ach) {
+							if (ach.completed) {
+								var dt = new Date(ach.completed);
+								var monthid = ''+dt.getFullYear()+((dt.getMonth() < 9)?'0':'')+(dt.getMonth()+1);
+
+								if (!achByMonths[monthid]) {
+									achByMonths[monthid] = new Array(31);
+								}
+								if (!achByMonths[monthid][dt.getDate()]) {
+									achByMonths[monthid][dt.getDate()] = [];
+								} 
+								
+								achByMonths[monthid][dt.getDate()].push(ach);
+
+								achByMonths[monthid].hasAchievements = true;
+							}
+						});		
+					});
+				});
+			});
+
 			var index = 0;
+			var foundFirstMonth = false;
 			for (var year = 2008; year <= today.getFullYear(); year++) {
 				for (var month = 1; month <= 12; month++) {
 					var monthid = '' + year + ((month < 10)?'0':'') + month;
-					
-					//''+year+((dt.getMonth() < 9)?'0':'')+(dt.getMonth()+1);
+					var thisMonth = (year === today.getFullYear()) && (month === (today.getMonth()+1));
 
-					// TODO: don't include early months if there were not achievements for that month
-					/*if ((!isfirstmonth) || (typeof calendar[''+yearx+monthid] != 'undefined')) {
-						calenpages.push(''+yearx+monthid);
-						isfirstmonth = false;
-						if ((yearx == today.getFullYear()) && (monthx == (today.getMonth()+1))) break;
+					// if we're still trying to find the first month, don't include a bunch of months that don't have achievements
+					if (!foundFirstMonth && (!achByMonths[monthid] || !achByMonths[monthid].hasAchievements)) {
+						continue;
 					}
-					*/
+					foundFirstMonth = true;
 					
-					var thisMonth = (year == today.getFullYear()) && (month == (today.getMonth()+1));
-
 					// Add the months to the list of months
 					months.push({
 						value:monthid,
@@ -72,14 +99,20 @@
 						index: index++
 					});
 
-					// Add this month to the calendar html
-					var rowData = buildRows(month, year);
-					calendarHtml += '<table class="calendar' + (thisMonth ? " curCalendar" : "") + '" id="calendar'+ monthid +'" style="display: ' + (thisMonth ? "block" : "none")+ '" total="' + rowData.total + '" points="' + rowData.points + '"">';
+					// build up the table information
+					var rowData = buildRows(month, year, achByMonths);
+
+					// Add the table to the html
+					calendarHtml += '<table class="calendar' + (thisMonth ? ' curCalendar' : '') + 
+						'" id="calendar'+ monthid +'" style="display: ' + (thisMonth ? 'block' : 'none')+ 
+						'" total="' + rowData.total + '" points="' + rowData.points + '"">';
 					calendarHtml += rowData.rows;
 					calendarHtml += '</table>';
 
 					// Stop once we get to this month
-					if (thisMonth) break;
+					if (thisMonth) {
+						break;
+					}
 				}
 			}
 
@@ -87,41 +120,69 @@
     		$scope.months = months;
     	}
 
-    	function buildRows(month, year) {
-			var rows = "";
+    	function buildRows(month, year, achByMonths) {
+			var rows = '';
 		    var total = 0;
 		    var points = 0;
+		    var d;
+
+		    // tmp
+		    var name = 'kevinc';
+
+			var lookup = '' + year + ((month < 10)?'0':'') + month;
+
 		    for (var day = 1; day <= 31; day++) {
-		        var d = new Date(year, month - 1, day);
-		        if (d.getDate() != day) break;
-		        if ((day == 1) || (d.getDay() == 0)) rows += '<tr>';
-		        if ((day == 1) && (d.getDay() > 0)) rows += '<td colspan="'+(d.getDay())+'" class="dayspacer"></td>';
+		        d = new Date(year, month - 1, day);
+		        if (d.getDate() !== day) {
+		        	break;
+		        }
+		        if ((day === 1) || (d.getDay() === 0)) {
+		        	rows += '<tr>';	
+		        } 
+		        if ((day === 1) && (d.getDay() > 0)) {
+		        	rows += '<td colspan="'+(d.getDay())+'" class="dayspacer"></td>';
+		        }
 		        rows += '<td>' + day;
 
 		        // TODO: build out achieve data
-		        // if ((typeof calendarData != 'undefined') && (typeof calendarData[day] != 'undefined')) {
-		        //     day = calendarData[day];
-		        //     day.sort(achdatesort);
-		        //     rows += '<div>'
-		        //     for (achx in day) {
-		        //         ach = day[achx];
-		        //         rows += '<a href="http://www.wowhead.com/achievement='+ach.id+'" rel="who=' + latestprofile.name + '&amp;when='+ach.completed+'"><img src="http://wow.zamimg.com/images/wow/icons/medium/'+ach.icon.toLowerCase()+'.jpg" width="36" height="36" border="0"></a>';
+		        if (achByMonths[lookup] && achByMonths[lookup][day]) {
+		        	var  achievs = achByMonths[lookup][day];
+		        	achievs.sort(achievementDaySort);
 
-		        //         total++;
-		        //         points += ach.points;
-		        //     }
-		        //     rows += '</div>';
-		        // }
+			        rows += '<div>'
+			        angular.forEach(achievs, function(ach) {
+			            rows += '<a href="http://www.wowhead.com/achievement=' + ach.id + '" ' +
+			        			'rel="who=' + name + '&amp;when=' + ach.completed +'">' +
+			        			'<img src="http://wow.zamimg.com/images/wow/icons/medium/' + 
+			        			ach.icon.toLowerCase() + '.jpg" width="36" height="36" border="0"></a>';
+
+			            total++;
+						points += ach.points;
+					});
+			        rows += '</div>';
+		        }
 
 		        rows += '</td>';
-		        if (d.getDay() == 6) rows += '</tr>';
+		        if (d.getDay() === 6) {
+		        	rows += '</tr>';	
+		        } 
 		    }
-		    if (d.getDay() < 6) rows += '<td colspan="'+(6-d.getDay())+'" class="dayspacer"></td>';
+		    if (d.getDay() < 6) {
+		    	rows += '<td colspan="'+(6-d.getDay())+'" class="dayspacer"></td>';
+		    }
 
-		    rows += "</tr>";
+		    rows += '</tr>';
 
-		    return { "rows": rows, "total": total, "points": points };
+		    return { 'rows': rows, 'total': total, 'points'	: points };
     	}
     }
+
+    function achievementDaySort(achieve1, achieve2) {
+		if (achieve1.completed === achieve2.completed) {
+			return (parseInt(achieve1.id,10) < parseInt(achieve2.id,10)) ? -1 : 1;
+		}
+
+		return (achieve1.completed < achieve2.completed) ? -1 : 1;
+	}
 
 })();
