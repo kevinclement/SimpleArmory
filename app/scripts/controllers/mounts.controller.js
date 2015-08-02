bac3f1ee722709bbcaa2b6c0f915d4f3b222d18f
Kevin Clement <kevin.clement@gmail.com>
2015-08-01 19:55:12 -0700

    Cleaned out hardcoding

--- app/scripts/controllers/mounts.controller.js
+++ app/scripts/controllers/mounts.controller.js
@@ -18,24 +18,28 @@
             $scope.isCollected = function (id) {
                 return items.lookup[id] !== undefined;
             };
+            $scope.anchorCss = function (step) {
+                if (step.epic) {
+                    return 'mnt-plan-epic';
+                }
+
+                return 'mnt-plan-rare';
+            };
+            $scope.getPlanImageSrc = function (step) {
+                if (!step.icon) {
+                    return '';
+                }
+
+                return '//wow.zamimg.com/images/wow/icons/tiny/' + step.icon + '.gif';
+            };
 
             $scope.planner = 
             [
                 {step:'Start off in Stormwind/Orgrimmar'},
                 {step:'Portal to Uldum'},
-                {step:'Run Vortex Pinnacle', boss:'Altarius', spellId:88472, itemId:63040, notes:'You can run heroic once and 9 normal'},
-                {step:'Run Throne of the Four Winds', boss:'Al\'akir', spellId:88744, itemId:63041},
+                {step:'Run Vortex Pinnacle', boss:'Altarius', spellId:88472, itemId:63040, name: 'Foo', icon:'inv_misc_summerfest_braziergreen', notes:'You can run heroic once and 9 normal'},
+                {step:'Run Throne of the Four Winds', boss:'Al\'akir', spellId:88744, itemId:63041, name: 'Bar', epic:true, icon: 'ability_mount_drake_blue'},
             ];
-
-            // TODO: what is the best way to do this??
-
-            // refresh the wow links so they are stylized and iconized
-            //if ($WowheadPower && $scope.showPlanner) {
-                //window.setTimeout(function() {
-                //    wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": true };
-                //    $WowheadPower.refreshLinks();
-                //}, 0);
-            //}
         });       
     }
 
