90789131f72c37fa4855338e5ecca21acfe75f8c
Kevin Clement <kevin.clement@gmail.com>
2016-07-31 17:09:52 -0700

    Target _blank instead of _self for anchors
    Also added support for this as a setting in case I want to change it back, or perhaps make it based on  user pref
    
    Fixes #90

--- app/scripts/services/settings.service.js
+++ app/scripts/services/settings.service.js
@@ -13,6 +13,7 @@
             'apiEndPoint':'api.battle.net',
             'apiKey': '&apikey=kwptv272nvrashj83xtxcdysghbkw6ep',
             'apiProtocol': 'https://',
+            'anchorTarget': '_blank',  // in case we want this to be a setting for _self
             'debug': $routeParams['debug'] && $routeParams['debug'] === '1' ? true : false,
             'fakeCompletionTime': 312
         };       
