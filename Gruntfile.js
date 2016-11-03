2e1cb844f23b562c15c3d7f47c01e76d03a44019
Dmytro Kovtun <dmytro_kovtun@epam.com>
2016-11-03 14:44:13 +0200

    7.1 Mounts added to mount list. Small bug fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,8 @@ module.exports = function (grunt) {
         tasks: ['wiredep']
       },
       js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        files: ['<%= yeoman.app %>/scripts/{,*/}*.js',
+		        '<%= yeoman.app %>/data/{,*/}*.json'],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: '<%= connect.options.livereload %>'
