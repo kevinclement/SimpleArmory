da1f7de0dadd2d27f56bca9cf698f2fb35b818e5
Kevin Clement <kevin.clement@gmail.com>
2017-10-03 18:31:39 -0700

    super simple drag/drop support

---
+++ app/scripts/directives/dragdrop.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function() {
+
+    angular
+        .module('simpleArmoryApp')
+        .directive('ngDrag', ngDrag)
+        .directive('ngDrop', ngDrop);
+
+    function ngDrag() {
+        return function (scope, element, attrs) {
+            element.attr("draggable", true);
+            element.bind("dragstart", function (e) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngDrag);
+                });
+            });
+        };
+    }
+
+    function ngDrop() {
+        return function (scope, element, attrs) {
+            element.bind("dragover", function(e) {
+                if (e.preventDefault) {
+                    e.preventDefault();
+                }
+
+                e.originalEvent.dataTransfer.dropEffect = 'move';
+                return false;
+            });
+
+            element.bind("drop", function (e) {
+                 scope.$apply(function (){
+                     scope.$eval(attrs.ngDrop);
+                 });
+            });
+
+            /* Add/Remove css class on enter/exit if specified*/
+            if (attrs.ngDropClass) {
+                element.bind("dragenter", function (e) {
+                    e.target.classList.add(attrs.ngDropClass);
+                });
+
+                element.bind("dragleave", function (e) {
+                    e.target.classList.remove(attrs.ngDropClass);
+                });
+            }
+        };
+    }
+    
+})();
