7f4e54836958d483a8249a30e9128a8bfdb28ba7
Kevin Clement <kevin.clement@gmail.com>
2017-10-06 20:05:12 -0700

    dynamically add achievement sections to admin section

--- app/scripts/controllers/admin.controller.js
+++ app/scripts/controllers/admin.controller.js
@@ -6,24 +6,38 @@
         .module('simpleArmoryApp')
         .controller('AdminCtrl', AdminCtrl);
 
-    function AdminCtrl($scope, $window, $routeParams, SettingsService, $location) {
+    function AdminCtrl($scope, $window, $routeParams, SettingsService, $location, AdminService) {
 
         // Analytics for page
         $window.ga('send', 'pageview', 'Admin');
+
+        // Store url parts into scope
+        $scope.area = $routeParams.area;            // e.g. categories
+        $scope.section = $routeParams.section       // e.g. achievement
+        $scope.subsection = $routeParams.subsection // e.g. quests
 
         // Use the settings to materialize select drop down
         $scope.sections = [
             { 'label': 'Mounts', 'route': '/admin/mounts' },
             { 'label': 'Mount Categories', 'route': '/admin/categories/mounts' },
-            { 'label': 'Achievement Categories (General)', 'route': '/admin/categories/achievements/general' },
-            { 'label': 'Achievement Categories (Quests)', 'route': '/admin/categories/achievements/quests' }
-        ]
+        ];
 
-        // find the matching drop down so we can select it when loading from url
-        angular.forEach($scope.sections, function(section) {
-            if (section.route === $location.path()) {
-                $scope.selectedAdminSection = section;
+        // Pull all the achievement super categories out and add them to sections
+        AdminService.getAchievementData().then(function(data){
+            for(var i=0; i < data.supercats.length; i++) {
+                var supercat = data.supercats[i];
+                var label = 'Achievement - ' + supercat.name;
+                var route = '/admin/categories/achievements/' + supercat.name.toLowerCase();
+
+                $scope.sections.push({ 'label': label, 'route': route });
             }
+
+            // find the matching drop down so we can select it when loading from url
+            angular.forEach($scope.sections, function(section) {
+                if (section.route === $location.path()) {
+                    $scope.selectedAdminSection = section;
+                }
+            });
         });
 
         $scope.canSave = function(file, data) {
@@ -35,11 +49,6 @@
             // selection changes url
             $location.url(newValue.route);
         });
-
-        // Store url parts into scope
-        $scope.area = $routeParams.area;            // e.g. categories
-        $scope.section = $routeParams.section       // e.g. achievement
-        $scope.subsection = $routeParams.subsection // e.g. quests
 
         $scope.saveClicked = function() {
             // NOTE: There is probably an easier way todo this, but I'm using 2 anchors, one to trigger refresh of data
