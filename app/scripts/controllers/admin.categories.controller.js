85a407ecd5264b89c493330dd35aec0c12984f07
Kevin Clement <kevin.clement@gmail.com>
2017-10-08 01:07:14 -0700

    use generic controller for achievements now

--- app/scripts/controllers/admin.categories.controller.js
+++ app/scripts/controllers/admin.categories.controller.js
@@ -9,31 +9,68 @@
 
     function AdminCategories($scope, AdminService) {
 
-        AdminService.getMountData().then(function(data){
+        // TODO: add link to wowhead like old achievement had
 
-            // Generic interface to allow different types of column objects
-            $scope.col1items = data;
-            $scope.col1child = 'subcats';
-            $scope.col1Title = 'Category';
-            $scope.col1Factory = function(newCol1) {
-                return { name: newCol1, subcats: [], id: createSimpleGuid().toString() };
-            }
+        if ($scope.section === 'mounts') {
+            AdminService.getMountData().then(function(data){
 
-            $scope.col2Title = 'Sub Category';
-            $scope.col2Factory = function(newCol2) {
-                return { name: newCol2, items: [], id: createSimpleGuid().toString() };
-            }
+                $scope.col1items = data;
+                $scope.col1child = 'subcats';
+                $scope.col1Title = 'Category';
+                $scope.col1Factory = function(newCol1) {
+                    return { name: newCol1, subcats: [], id: createSimpleGuid().toString() };
+                }
 
-            $scope.col3Title = 'Item';
-            $scope.col3Label = function(col3item) {
-                return col3item.icon;
-            }
-            $scope.col3child = 'items';
+                $scope.col2Title = 'Sub Category';
+                $scope.col2Factory = function(newCol2) {
+                    return { name: newCol2, items: [], id: createSimpleGuid().toString() };
+                }
 
-            // TODO: either switch out here, or move out one layer and do this in admin controller
+                $scope.col3Title = 'Item';
+                $scope.col3Label = function(col3item) {
+                    return col3item.icon;
+                }
+                $scope.col3child = 'items';
 
-            $scope.selectionChanged(true);
-        });
+                $scope.selectionChanged(true);
+            });
+        } else if ($scope.section === 'achievements') {
+            AdminService.getAchievementData().then(function(data){
+                var categories = [];
+                for(var i=0; i < data.supercats.length; i++) {
+                    if (data.supercats[i].name.toLowerCase() === $scope.subsection) {
+                        for(var j=0; j < data.supercats[i].cats.length; j++) {
+                            // data.supercats is array with super cats
+                            // data[0].cats are the categories
+                            // data[0].cats[0].zones are the subcats
+                            // data[0].cats[0].zones[0].achs are the items
+    
+                            categories.push(data.supercats[i].cats[j]);
+                        }
+                    }
+                }
+
+                $scope.col1items = categories;
+                $scope.col1child = 'zones';
+                $scope.col1Title = 'Category';
+                $scope.col1Factory = function(newCol1) {
+                    return { name: newCol1, zones: [], id: createSimpleGuid().toString() };
+                }
+
+                $scope.col2Title = 'Zone';
+                $scope.col2Factory = function(newCol2) {
+                    return { name: newCol2, achs: [], id: createSimpleGuid().toString() };
+                }
+
+                $scope.col3Title = 'Achievement';
+                $scope.col3Label = function(col3item) {
+                    return col3item.id;
+                }
+                $scope.col3child = 'achs';
+
+                $scope.selectionChanged(true);
+            });
+        }
 
         $scope.selectionChanged = function(indexChanged) {
 
