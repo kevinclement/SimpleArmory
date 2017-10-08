c8ec5d5f07c3895405381f564fcdaad7c1dba778
Kevin Clement <kevin.clement@gmail.com>
2017-10-08 00:38:31 -0700

    unify naming and remove

--- app/scripts/controllers/admin.categories.controller.js
+++ app/scripts/controllers/admin.categories.controller.js
@@ -10,23 +10,10 @@
     function AdminCategories($scope, AdminService) {
 
         AdminService.getMountData().then(function(data){
-            var col1 = [];
-            for(var i=0; i < data.length; i++) {
-                col1.push(data[i]);
-            }
 
             // Generic interface to allow different types of column objects
-            $scope.col1 = col1;
-            $scope.selectedCol1 = $scope.col1[0];
-            $scope.col1Children = function(col1item) {
-                return col1item.subcats;
-            }
-            $scope.col2Children = function(col2item) {
-                return col2item.items;
-            }
-            $scope.col3Label = function(col3item) {
-                return col3item.icon;
-            }
+            $scope.col1items = data;
+
             $scope.col1Factory = function(newCol1) {
                 return { name: newCol1, subcats: [], id: createSimpleGuid().toString() };
             }
@@ -46,41 +33,47 @@
         });
 
         $scope.selectionChanged = function(indexChanged) {
-            var col1 = $scope.selectedCol1;
-            var col2 = $scope.selectedCol2;
-            var col3 = $scope.selectedCol3;
 
-            if (col1 === null) {
-                $scope.selectedCol1 = col1 = $scope.col1[0];
+            if ($scope.col1selected === undefined) {
+                $scope.col1selected = $scope.col1items[0];
             }
 
-            if (col2 === undefined || 
-                $scope.col1Children(col1).indexOf(col2) === -1) {
-                $scope.selectedCol2 = col2 =  $scope.col1Children(col1)[0];
+            // TODO: use subcats or something else
+            $scope.col2items = $scope.col1selected.subcats;
+            if ($scope.col2selected === undefined || 
+                $scope.col2items.indexOf($scope.col2selected) === -1) {
+                $scope.col2selected = $scope.col2items[0];
             }
 
-            if (col3 === undefined || 
-                $scope.col2Children(col2).indexOf(col3) === -1) {
-                $scope.selectedCol3 = col3 = $scope.col2Children(col2)[0];
+            $scope.col3items = $scope.col2selected.items;
+            if ($scope.col3selected === undefined || 
+                $scope.col3items.indexOf($scope.col3selected) === -1) {
+                $scope.col3selected = $scope.col3items[0];
             }
 
             // enable and disable up/down arrows if we're at the boundaries
-            $scope.catUpDisabled = $scope.col1.indexOf(col1) === 0;
-            $scope.catDownDisabled = $scope.col1.indexOf(col1) === $scope.col1.length - 1;
+            $scope.catUpDisabled = $scope.col1items.indexOf($scope.col1selected) === 0;
+            $scope.catDownDisabled = $scope.col1items.indexOf($scope.col1selected) === $scope.col1items.length - 1;
 
-            $scope.subCatUpDisabled = $scope.col1Children(col1).indexOf(col2) === 0;
-            $scope.subCatDownDisabled =  $scope.col1Children(col1).indexOf(col2) ===  $scope.col1Children(col1).length - 1;
+            $scope.subCatUpDisabled = $scope.col2items.indexOf($scope.col2selected) === 0;
+            $scope.subCatDownDisabled =  $scope.col2items.indexOf($scope.col2selected) === $scope.col2items.length - 1;
 
-            $scope.itemUpDisabled = $scope.col2Children(col2).indexOf(col3) === 0;
-            $scope.itemDownDisabled = $scope.col2Children(col2).indexOf(col3) === $scope.col2Children(col2).length - 1;
+            $scope.itemUpDisabled = $scope.col3items.indexOf($scope.col3selected) === 0;
+            $scope.itemDownDisabled = $scope.col3items.indexOf($scope.col2selected) === $scope.col3items.length - 1;
 
             // if called from an index change, then don't mark it dirty
             if (!indexChanged) {
-                $scope.$parent.canSave('mounts.json', $scope.col1);
+                $scope.$parent.canSave('mounts.json', $scope.col1items);
             }
         };
 
-        $scope.move = function(up, item, array) {
+        $scope.col3Label = function(col3item) {
+            return col3item.icon;
+        }
+
+        $scope.move = function(up, item, parent) {
+            var array = parent.subcats ? parent.subcats : parent.items;
+
             var src = array.indexOf(item);
             var dest = up ? src - 1 : src + 1;
 
@@ -100,20 +93,18 @@
             }
         }
 
-        $scope.remove = function(colArray, selectedItem, scopeVar) {
-
-            $scope[scopeVar] = colArray.filter(function(item){
-                return item !== selectedItem;
+        $scope.removeCol1 = function() {
+            $scope.col1items = $scope.col1items.filter(function(item){
+                return item !== $scope.col1selected;
             });
-
+            $scope.col1selected = $scope.col1items[0];
+            
             $scope.selectionChanged();
         };
 
-        /* ## Sub Category ############################################################################### */
-
-        $scope.removeSubCategory = function() {
-            $scope.selectedCat.subcats = $scope.selectedCat.subcats.filter(function(sub){
-                return sub !== $scope.selectedSubCat;
+        $scope.removeCol2 = function() {
+            $scope.col1selected.subcats = $scope.col1selected.subcats.filter(function(item){
+                return item !== $scope.col2selected;
             });
 
             $scope.selectionChanged();
