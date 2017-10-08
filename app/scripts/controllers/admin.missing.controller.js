396be3f83d1aea66541963c7ecf147228f36242d
Kevin Clement <kevin.clement@gmail.com>
2017-10-08 14:52:13 -0700

    prep for achievements

--- app/scripts/controllers/admin.missing.controller.js
+++ app/scripts/controllers/admin.missing.controller.js
@@ -15,21 +15,26 @@
         // TODO: will need to add ids to achievement categories
         // TODO: fix route to including missing
 
-        AdminService.getMountData().then(function(data){
-            var categories = [];
-
-            for(var i=0; i<data.length; i++) {
-                var cat = data[i];
-
-                categories.push(cat);
-            }
-
-            $scope.categories = categories;
-        });
-
-        AdminService.getMissingMounts().then(function(data){
-            $scope.missing = data;
-        });
+        // TODO: have support for both mount and achievements
+        if ($scope.section === 'mounts') {
+            AdminService.getMountData().then(function(data){
+                var categories = [];
+    
+                for(var i=0; i<data.length; i++) {
+                    var cat = data[i];
+    
+                    categories.push(cat);
+                }
+    
+                $scope.categories = categories;
+            });
+    
+            AdminService.getMissingMounts().then(function(data){
+                $scope.missing = data;
+            });
+        } else if ($scope.section === 'achievements') {
+            
+        }
 
         var draggedItem;
         $scope.dragStart = function(dragItem) {
