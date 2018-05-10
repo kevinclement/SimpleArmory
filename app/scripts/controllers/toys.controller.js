c8e1daefeece47e2e33acc10215a0bb4b1608a88
Antoine Pietri <antoine.pietri1@gmail.com>
2018-05-10 22:43:27 +0200

    Add a Toys section that relies on an import string

---
+++ app/scripts/controllers/toys.controller.js
@@ -0,0 +1,40 @@
+/*globals $WowheadPower */
+'use strict';
+
+(function() {
+
+    angular
+        .module('simpleArmoryApp')
+        .controller('ToysCtrl' , ToysCtrl);
+
+    function ToysCtrl($scope, MountsAndPetsService, PlannerService, $window, SettingsService) {
+
+        $scope.settings = SettingsService;
+
+    	// Analytics for page
+        $window.ga('send', 'pageview', 'Toys');
+
+        MountsAndPetsService.getItems('toys', 'toys', 'itemId').then(function(items){
+            $scope.items = items;
+        });
+    }
+
+  angular
+    .module('simpleArmoryApp')
+    .controller('ToysFormCtrl' , ToysFormCtrl);
+
+  function ToysFormCtrl($scope) {
+    $scope.formInfo = {};
+    $scope.saveData = function() {
+      try {
+        JSON.parse($scope.formInfo.toys);
+      } catch (e) {
+        // TODO: display json error, do more consistency checks
+        return;
+      }
+      localStorage.setItem('toys', $scope.formInfo.toys);
+      window.location.reload(true);
+    };
+  }
+
+})();
