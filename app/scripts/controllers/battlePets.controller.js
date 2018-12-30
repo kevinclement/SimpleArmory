8530772f653992c349c6c120446962d5fe276ebd
Antoine Pietri <antoine.pietri1@gmail.com>
2018-12-30 19:36:20 +0100

    companions: add rarity background
    Fixes #40

--- app/scripts/controllers/battlePets.controller.js
+++ app/scripts/controllers/battlePets.controller.js
@@ -16,27 +16,6 @@
         MountsAndPetsService.getItems('battlepets', 'pets', 'creatureId').then(function(items){
             $scope.items = items;
         });
-
-        $scope.qualityToBackground = function(item) {
-            var bgColor = '#fff';
-
-            switch(item.quality) {
-                case 'poor':
-                    bgColor = '#7F7F7F';
-                    break;
-                case 'common':
-                    bgColor = '#F0F0F0';
-                    break;
-                case 'uncommon':
-                    bgColor = '#22B14C';
-                    break;
-                case 'rare':
-                    bgColor = '#3F48CC';
-                    break;
-            }
-
-            return 'background:' + bgColor;
-        };
     }
 
 })();
