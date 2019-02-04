1623ded0241f7c186c2bb839e4f16308e3fbeeed
Bláckfur <b.van.rheenen@bmcr.nl>
2019-02-04 18:35:10 +0100

    mounts: added feature to toggle unobtainable mounts

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -1,3 +1,36 @@
 'use strict';
 
-/* Filters */
+(function() {
+    angular
+    	.module('simpleArmoryApp')
+        .filter('filterUnobtainables', filterUnobtainables);
+
+	/* Filters */
+	function filterUnobtainables() {
+
+		return function (category, unobtainable) {
+			var filteredCatergory = [];
+
+		    for(var i = 0; i < category.length; i++) {
+		    	if(!unobtainable) {
+					unobtainable = undefined;
+				}
+
+				// Proceed if some mounts in the sub-category are obtainable OR
+				// if every sub-category mount is unobtainable and the 'show unobtainables' option is enabled
+		    	if(
+		    		category[i].items.some(function(subCategory) { return subCategory.notObtainable === unobtainable || subCategory.collected; }) || 
+		    		category[i].items.every(function(subCategory) { return subCategory.notObtainable !== unobtainable; }) && 
+		    		unobtainable
+		    	) {	
+		    		// Proceed if all the mounts are currently in-game
+		    		if(category[i].items.some(function(subCategory) { return !subCategory.notIngame; })) {
+		    			filteredCatergory.push(category[i]);	
+		    		}	
+		    	}
+		    }
+
+		    return filteredCatergory;
+		};
+	}
+})();
