29bee47836ade535135f84403fef624fec99713e
Kevin Clement <kevin.clement@gmail.com>
2020-03-18 20:44:06 -0700

    update pets json to include id of pets

---
+++ data_scripts/pets_fixer_wowtools.py
@@ -0,0 +1,57 @@
+
+#!/usr/bin/env python3
+
+import csv
+import json
+import logging
+import sys
+
+
+def changelog(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
+class PetsCreatureIdFixer:
+    def __init__(self, pets, creatureid_to_id):
+        self.pets = pets
+        self.creatureid_to_id = creatureid_to_id
+
+    def run(self):
+        found_ids = {}
+        for cat in self.pets:
+            for subcat in cat['subcats']:
+                for item in subcat['items']:
+                    cid = item['creatureId']
+                    if (cid in self.creatureid_to_id):
+                        found_ids[cid] = True
+                        item['ID'] = self.creatureid_to_id[cid]
+                    #else:
+                        #print("DIDNT FIND ", cid)
+                        #json.dump(item, sys.stdout, indent=2, sort_keys=True)
+                        #print()
+
+                    #if (item['creatureId'] not in found_ids):
+                    #    creatureIds[item['creatureId']] = True
+                    #else:
+                    #    print("DUPE: ", item['creatureId'])
+
+        # dump json out of new json
+        json.dump(self.pets, sys.stdout, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    if len(sys.argv) < 2:
+        sys.exit("Usage: {} pets.json wowtools.pets.csv")
+
+with open(sys.argv[2]) as csvfile:
+        creatureid_to_id = {}
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #print(row['CreatureID'],"=>",row['ID'])
+            creatureid_to_id[int(row['CreatureID'])] = row['ID']
+
+        pets = json.load(open(sys.argv[1]))
+
+        fixer = PetsCreatureIdFixer(pets, creatureid_to_id)
+        fixer.run()
