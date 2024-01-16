1898da45e4dcb60646e1a28ffd50e23fe40e1279
Kevin Clement <kevin.clement@gmail.com>
2024-01-15 18:00:21 -0800

    fixing 10.2.5 mounts that were dupes

--- dataimporter/mounts.py
+++ dataimporter/mounts.py
@@ -47,6 +47,8 @@ IGNORE_MOUNT_ID = [
     1788,  # Azeroth Dragonriding Cliffside Wylderdrake: https://www.wowhead.com/mount/1788
     1789,  # Azeroth Dragonriding Winding Slitherdrake: https://www.wowhead.com/mount/1789
     1796,  # Temp: https://www.wowhead.com/mount/1796
+    1953,  # Grotto Netherwing Drake: https://www.wowhead.com/mount/1953 : dupe of 1744
+    1954,  # Flourishing Whimsydrake: https://www.wowhead.com/mount/1954 : dupe of 1830
 ]
 
 MOUNT_SOURCE_ENUM = {
