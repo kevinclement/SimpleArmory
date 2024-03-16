31e03cabc4260c8497c5fb4770dbc4c6ca3151cb
André Schwarzer <as@seitenweise.com>
2024-03-16 23:10:46 +0100

    Add progress bar for each category

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -1,22 +1,24 @@
-<script>   
+<script>
     export let width = 0;
     export let styleWidth = 250;
     export let percentage = "";
     export let rightSide = false;
     export let url = "";
     export let name = "";
+    export let isEmpty = false;
+    export let emptyValue = "";
 </script>
 
 <div class="progress" class:progressRight={rightSide} style="width: { styleWidth == 'auto' ? 'auto' : styleWidth + 'px' }">
 {#if url !== ""}
     <a href={url} aria-label="{name}">
         <div class="progress-bar progress-bar-success" style="width: {width}%;">
-            <span>{ percentage }</span>
+            <span>{ isEmpty ? emptyValue : percentage }</span>
         </div>
     </a>
 {:else}
     <div class="progress-bar progress-bar-success" style="width: {width}%;">
-        <span>{ percentage }</span>
+        <span>{ isEmpty ? emptyValue : percentage }</span>
     </div>
 {/if}
 </div>
