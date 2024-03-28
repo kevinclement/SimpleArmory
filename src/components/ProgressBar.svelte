c2bd49c7f740148cd07ec4ec8c33dcdb2cdfc88d
André Schwarzer <as@seitenweise.com>
2024-03-28 21:02:24 +0100

    Revert progress bar changes

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -5,20 +5,18 @@
     export let rightSide = false;
     export let url = "";
     export let name = "";
-    export let isEmpty = false;
-    export let emptyValue = "";
 </script>
 
 <div class="progress" class:progressRight={rightSide} style="width: { styleWidth == 'auto' ? 'auto' : styleWidth + 'px' }">
 {#if url !== ""}
     <a href={url} aria-label="{name}">
         <div class="progress-bar progress-bar-success" style="width: {width}%;">
-            <span>{ isEmpty ? emptyValue : percentage }</span>
+            <span>{ percentage }</span>
         </div>
     </a>
 {:else}
     <div class="progress-bar progress-bar-success" style="width: {width}%;">
-        <span>{ isEmpty ? emptyValue : percentage }</span>
+        <span>{ percentage }</span>
     </div>
 {/if}
 </div>
